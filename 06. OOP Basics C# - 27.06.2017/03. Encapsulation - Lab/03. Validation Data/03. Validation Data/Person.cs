621f60f6bd097f8f589604af090391c62b5085e5
VaskoViktorov <vaskoviktorov@gmail.com>
2017-07-01 18:32:43 +0300

    Encapsulation lab homework

---
+++ 06. OOP Basics C# - 27.06.2017/03. Encapsulation - Lab/03. Validation Data/03. Validation Data/Person.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private double salary;
+
+    public string FirstName
+    {
+        get { return this.firstName; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot be less than 3 symbols");
+            }
+            this.firstName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return this.lastName; }
+        set
+        {
+            if (value.Length < 3)
+            {
+                throw  new ArgumentException("Last name cannot be less than 3 symbols");
+            }
+            this.lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return this.age; }
+        set
+        {
+            if (value < 1)
+            {
+                throw new ArgumentException("Age cannot be zero or negative integer");
+            }
+            this.age = value;
+        }
+    }
+
+    public double Salary
+    {
+        get { return this.salary; }
+        set
+        {
+            if (value < 460)
+            {               
+                throw new ArgumentException("Salary cannot be less than 460 leva");
+            }
+            this.salary = value;
+        }
+    }
+
+    public Person(string firstName, string lastName, int age, double salary)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.firstName} {this.lastName} get {this.salary} leva";
+
+    }
+
+    public void IncreaseSalary(double bonus)
+    {
+        if (this.Age > 30)
+        {
+            this.Salary += this.Salary * (bonus / 100);
+        }
+        else
+        {
+            this.Salary += this.Salary * (bonus / 2 / 100);
+        }
+    }
+}
+
---
+++ 06. OOP Basics C# - 27.06.2017/03. Encapsulation - Lab/04. First and Reserve Team/04. First and Reserve Team/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+public class Program
+{
+    public static void Main()
+    {
+        var lines = int.Parse(Console.ReadLine());
+        
+        var team = new Team("new team");
+        
+        for (int i = 0; i < lines; i++)
+        {
+            var cmdArgs = Console.ReadLine().Split();
+            var person = new Person(cmdArgs[0],
+                cmdArgs[1],
+                int.Parse(cmdArgs[2]),
+                double.Parse(cmdArgs[3]));
+
+            
+            team.AddPlayer(person);
+        }
+
+        Console.WriteLine($"First team have {team.FirstTeam.Count} players");
+        Console.WriteLine($"Reserve team have {team.ReserveTeam.Count} players");
+    }
+}
+
