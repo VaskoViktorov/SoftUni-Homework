2189dea0a8f00532d3b4b8e1d3fc1b7150febb19
VaskoViktorov <vaskoviktorov@gmail.com>
2017-07-07 19:28:44 +0300

    Polymorphism - Lab - upload

---
+++ 06. OOP Basics C# - 27.06.2017/07. Polymorphism - Lab/02. Animals/02. Animals/Animal.cs
@@ -0,0 +1,26 @@
+﻿
+public class Animal
+{
+    private string name;
+    private string favouriteFood;
+
+    public Animal(string name, string favouriteFood)
+    {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+    public string FavouriteFood
+    {
+        get { return this.favouriteFood; }
+        set { this.favouriteFood = value; }
+    }
+    public virtual string ExplainMyself()
+    {
+        return $"I am {this.Name} and my favourite food is {this.favouriteFood}";
+    }
+}
