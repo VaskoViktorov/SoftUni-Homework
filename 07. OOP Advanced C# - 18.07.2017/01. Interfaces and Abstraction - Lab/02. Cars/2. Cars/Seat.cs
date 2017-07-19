bfb61894f00f6912ce3a26ebc7100df2f3c0298a
VaskoViktorov <vaskoviktorov@gmail.com>
2017-07-19 22:14:11 +0300

    Interfaces-Lab-Upload

---
+++ 07. OOP Advanced C# - 18.07.2017/01. Interfaces and Abstraction - Lab/02. Cars/2. Cars/Seat.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Text;
+
+public class Seat : ICar
+{
+    public Seat(string model, string color)
+    {
+        Model = model;
+        Color = color;
+    }
+
+    public string Model { get; protected set; }
+    public string Color { get; protected set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"{this.Color} Seat {this.Model}");
+        sb.AppendLine(Start());
+        sb.Append(Stop());
+
+        return sb.ToString();
+    }
+}
+
---
+++ 07. OOP Advanced C# - 18.07.2017/01. Interfaces and Abstraction - Lab/02. Cars/2. Cars/Tesla.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Text;
+
+public class Tesla : ICar, IElectricCar
+{
+    public Tesla(string model, string color, int battery)
+    {
+        Model = model;
+        Color = color;
+        Battery = battery;
+    }
+
+    public string Model { get; protected set; }
+
+    public string Color { get; protected set; }
+
+    public int Battery { get; protected set; }
+
+    public string Start()
+    {
+        return "Engine start";
+    }
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+        sb.AppendLine(Start());
+        sb.Append(Stop());
+
+        return sb.ToString();
+    }
+}
+
