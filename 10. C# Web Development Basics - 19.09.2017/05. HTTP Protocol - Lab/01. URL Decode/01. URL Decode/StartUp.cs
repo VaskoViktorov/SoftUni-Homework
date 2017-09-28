51d278f68163174d4ec662c9d4ef8524d2b6eeec
VaskoViktorov <vaskoviktorov@gmail.com>
2017-09-28 17:01:23 +0300

    HTTP Protocol Lab - upload

---
+++ 10. C# Web Development Basics - 19.09.2017/05. HTTP Protocol - Lab/01. URL Decode/01. URL Decode/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace _01._URL_Decode
+{
+    using System;
+    using System.Net;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string encodedUrl = Console.ReadLine();
+            string decodedUrl = WebUtility.UrlDecode(encodedUrl);
+
+            Console.WriteLine(decodedUrl);
+        }
+    }
+}
